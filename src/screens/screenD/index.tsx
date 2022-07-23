f74b8497128463bac6692573f5e5c76c5f423cf8
seyithan.topal <topal@sigo.green>
2022-07-23 22:15:11 +0300

    disabled back button

--- src/screens/screenD/index.tsx
+++ src/screens/screenD/index.tsx
@@ -1,19 +1,19 @@
-import React, { FC } from 'react';
-import { View, TouchableWithoutFeedback } from 'react-native';
+import React, { FC, useEffect } from 'react';
+import { BackHandler, View } from 'react-native';
 import { screenDProp } from '../../types/navigation';
 
-import LeftArrowIcon from '../../assets/icons/leftArrow';
 import TickIcon from '../../assets/icons/tick';
 import styles from './styles';
 
-const ScreenD: FC<screenDProp> = ({ navigation }) => {
+const ScreenD: FC<screenDProp> = () => {
+  useEffect(() => {
+    const backAction = (): boolean => true;
+    BackHandler.addEventListener('hardwareBackPress', backAction);
+    return () =>
+      BackHandler.removeEventListener('hardwareBackPress', backAction);
+  }, []);
   return (
     <View style={styles.wrapper}>
-      <TouchableWithoutFeedback onPress={() => navigation.goBack()}>
-        <View style={styles.leftArrowIcon}>
-          <LeftArrowIcon />
-        </View>
-      </TouchableWithoutFeedback>
       <View>
         <TickIcon />
       </View>
--- src/screens/screenD/styles.ts
+++ src/screens/screenD/styles.ts
@@ -8,18 +8,6 @@ export default StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#36d0ff',
   },
-  sub: {
-    width: '75%',
-  },
-  description: {
-    fontSize: 21,
-    textAlign: 'center',
-  },
-  leftArrowIcon: {
-    position: 'absolute',
-    left: 0,
-    top: 30,
-  },
   loadingIcon: {
     marginTop: 25,
   },
